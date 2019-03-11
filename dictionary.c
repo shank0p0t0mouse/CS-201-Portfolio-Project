a094b988aa9553119ea43117d8b6ca72956109f5
shank0p0t0mouse <ghdriskill@crimson.ua.edu>
2019-03-11 08:27:36 -0600

    Another commit including changes to dictionary.c to make it callable from Boggle.c.

--- dictionary.c
+++ dictionary.c
@@ -1,36 +1,39 @@
+#include <ncurses.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
+#include "dictionary.h"
 
-
-
- int main(){
-   char **dictionary;
-   dictionary=(char**)malloc(sizeof(char *)*(370200));
-   for(int x =0; x<370100; x++){
-     dictionary[x]=(char *)malloc(30);
-   }
+char **dictionary; //Global Variable holding the dictionary.
+void dictionaryLoad(){
+  ////////////////////////////////////////////////////
+  //Loads in dictionary from dictionary.txt.
+  dictionary=(char**)malloc(sizeof(char *)*(370200));
+  for(int x =0; x<370100; x++){
+    dictionary[x]=(char *)malloc(30);
+  }
   FILE *d = fopen("dictionary.txt", "r");
   if (d ==NULL){
-    printf("Error Opening File.\n");
-    return(-1);
-}
+   printf("Error Opening File.\n");
+   return;
+  }
 
-int i=0;
-while(!feof(d)){
-	fscanf(d, "%s", dictionary[i]);
-  i++;
-}
-
-char word[30]= "zuza";
-for (int y=0; y<370099; y++){
-  if (strcmp(word, dictionary[y])==0){
-    printf("%s   %s\n", dictionary[y], word);
+  int i=0;
+  while(!feof(d)){
+  fscanf(d, "%s", dictionary[i]);
+  if (strlen(dictionary[i])<3){
+  }
+  else{
+   i++;
   }
 }
-
-free(dictionary);
+  srand(time(NULL));
+  printf("%d",i);
   fclose(d);
 
-  return (0);
+
+
+
+  return;
 }
